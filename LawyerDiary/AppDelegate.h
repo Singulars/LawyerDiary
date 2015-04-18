8d24e34302923031a9d59ec8ff114aa5b200a101
naresh <nareshkharecha@gmail.com>
2015-04-19 02:05:32 +0530

    Initial commit.

---
+++ LawyerDiary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LawyerDiary
+//
+//  Created by Verma Mukesh on 19/04/15.
+//  Copyright (c) 2015 Singularsllp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
